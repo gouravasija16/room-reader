1527aa7f468d7ed993bf5b5f7fcba24c8e0ef826
Gourav <gouravasija1604@gmail.com>
2026-09-13 00:13:17 +0530

    Working on creator functionality

--- src/components/pages/MyRooms.jsx
+++ src/components/pages/MyRooms.jsx
@@ -1,5 +1,16 @@
+import useLocalStorage from "../shared/useLocalStorage"
+import { Link } from "react-router-dom"
+
 export default function MyRooms(){
+    const [myRooms,setMyRooms]=useLocalStorage("my-rooms",[])
     return(
-        <h2>here are your rooms</h2>
+        <>
+            {myRooms.length === 0 &&
+                <div className="flex flex-col gap-3 justify-center items-center my-auto py-3 ">
+                    <p className="text-2xl text-text font-bold">No rooms yet-Create your first rooms</p>
+                    <Link to="/create" className="text-lg border border-border rounded-2xl  text-accent inline-block w-48 mx-auto text-center ">Create your Room</Link>
+                </div>
+            }
+        </>
     )
 }
--- src/components/shared/RoomCard.jsx
+++ src/components/shared/RoomCard.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import DifficultyBadge from "./DifficultyBadge";
-export default  function RoomCard({key,room}){
+export default function RoomCard({key,room}){
     return(
          <div className="bg-elevated flex flex-col gap-5 rounded-lg px-6 py-5 border border-border transition hover:border-accent hover:scale-90 " key={key}>
           <h2 className="font-semibold text-lg text-text ">{room.title}</h2>
@@ -9,7 +9,6 @@ export default  function RoomCard({key,room}){
           <Link to={`/rooms/${room.id}`} className="px-6 py-3 rounded-lg font-medium transition hover:text-accent" >
               View Room
           </Link>
-        
          </div>
     ) 
 }
